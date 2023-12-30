4eeccd70a298c84d04be518cada56b0cd11f5c4d
Alan Kadzhaev <alan@Alans-MacBook-Pro.local>
2023-12-30 08:15:13 -0500

    Created a solution for the GCD game

---
+++ games/brain-gcd.js
@@ -0,0 +1,20 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber, launchGame } from '../src/index.js';
+
+const generateQuestionAndAnswer = () => {
+    const randomNumberFirst = getRandomNumber();
+    const randomNumberSecond = getRandomNumber();
+    const getGcd = (a, b) => {return (!b) ? a : getGcd(b,a%b)};
+    let correctAnswer =  getGcd(randomNumberFirst, randomNumberSecond);
+
+    const question = `Question: ${randomNumberFirst} ${randomNumberSecond}`;
+    return [question, correctAnswer];
+}
+
+const getUserResponse = () => {
+    const [question, correctAnswer] = generateQuestionAndAnswer();
+    const userAnswer = readlineSync.question(`${question}\n`);
+    return [Number(userAnswer), correctAnswer];
+};
+
+launchGame(getUserResponse);
